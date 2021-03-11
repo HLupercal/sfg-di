953fcd2ce413b43a20ae6a0822e9bf7295ea895b
lupercal <lupercaldev@gmail.com>
2021-03-11 15:38:08 +0100

    move primary bean config to java based configuration

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -1,13 +1,21 @@
 package guru.springframework.sfgdi.config;
 
 import guru.springframework.sfgdi.services.ConstructorGreetingService;
+import guru.springframework.sfgdi.services.PrimaryGreetingService;
 import guru.springframework.sfgdi.services.PropertyInjectedGreetingService;
 import guru.springframework.sfgdi.services.SetterInjectedGreetingService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class GreetingServiceConfig {
+
+    @Primary
+    @Bean
+    public PrimaryGreetingService primaryGreetingService(){
+        return new PrimaryGreetingService();
+    }
 
     @Bean
     public ConstructorGreetingService constructorGreetingService() {
