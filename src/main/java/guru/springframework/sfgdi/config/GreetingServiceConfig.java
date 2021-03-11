be4d88004858d2984b9f7afa4dcc86494659354a
lupercal <lupercaldev@gmail.com>
2021-03-11 15:30:12 +0100

    redo prop and setter injected services to use spring config

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -1,6 +1,8 @@
 package guru.springframework.sfgdi.config;
 
 import guru.springframework.sfgdi.services.ConstructorGreetingService;
+import guru.springframework.sfgdi.services.PropertyInjectedGreetingService;
+import guru.springframework.sfgdi.services.SetterInjectedGreetingService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,5 +12,15 @@ public class GreetingServiceConfig {
     @Bean
     public ConstructorGreetingService constructorGreetingService() {
         return new ConstructorGreetingService();
+    }
+
+    @Bean
+    public PropertyInjectedGreetingService propertyInjectedGreetingService() {
+        return new PropertyInjectedGreetingService();
+    }
+
+    @Bean
+    public SetterInjectedGreetingService setterInjectedGreetingService(){
+        return new SetterInjectedGreetingService();
     }
 }
